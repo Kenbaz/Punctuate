26394c4501575e95adfeca5d9345e6769e086de4
Kenbaz <kbassey016@gmail.com>
2024-06-21 17:34:21 +0100

    added few comments

--- src/components/PunctuationApp.tsx
+++ src/components/PunctuationApp.tsx
@@ -1,63 +1,64 @@
 import { useState } from "react";
 import { Helmet } from "react-helmet-async";
-import '../App.css';
+import "../App.css";
 import TextInput from "./TextInput";
 import ResultOutput from "./ResultOutput";
 import punctuateTextLineByLine from "../services/punctuationService";
 import useDetectWindowSize from "../useDetectWindowSize";
 
 const PunctuateApp: React.FC = () => {
-    const [result, setResult] = useState<string[]>([]); // state to handle the proccessed text
-    const [loading, setLoading] = useState(false); // state to handle loading state
-  const [error, setError] = useState('') // state to handle errors
-  const [text, setText] = useState('');
-  const { width } = useDetectWindowSize();
+  const [result, setResult] = useState<string[]>([]); // state to handle the proccessed text
+  const [loading, setLoading] = useState(false); // state to handle loading state
+  const [error, setError] = useState(""); // state to handle errors
+  const [text, setText] = useState("");
+  const { width } = useDetectWindowSize(); // Destructure width from useDetectWindowSize
+  
   const isMobile = width <= 912;
 
-    // Handle text submission
-    const handleTextSubmit = async (lines: string[]) => {
-        // Check if the input is empty 
-        const nonEmptyLines = lines.filter((line) => line.trim() !== "");
-        
-        if (nonEmptyLines.length === 0) {
-            return;
-        }
+  // Handle text submission
+  const handleTextSubmit = async (lines: string[]) => {
+    // Check if the input is empty
+    const nonEmptyLines = lines.filter((line) => line.trim() !== "");
 
-        setLoading(true); // start laoding
-        setError('') // clear any previous errors
-        setResult([]); // clear previous results
+    if (nonEmptyLines.length === 0) {
+      return;
+    }
 
-      try {
-        const processedLines = await punctuateTextLineByLine(nonEmptyLines);
-        setResult(processedLines);
-        if (isMobile) {
-          setText(processedLines.join('\n'));
-        }
-      } catch (err) {
-        setError('Failed to process text')
-      } finally {
-        setLoading(false);
-        }
-    };
+    setLoading(true); // start laoding
+    setError(""); // clear any previous errors
+    setResult([]); // clear previous results
 
-    return (
-      <main className="h-screen overflow-hidden lg:overflow-auto lg:grid-container">
-        <Helmet title="punctuate"/>
-        <div className="App h-full">
-          <TextInput
-            onSubmit={handleTextSubmit}
-            text={text}
-            setText={setText}
-            loading={loading}
-          />
-          {isMobile && <ResultOutput result={result} />}
-          {error && <p className="text-gray-800">{error}</p>}
-        </div>
-        <div className="hidden lg:block">
-          <ResultOutput result={result} />
-        </div>
-      </main>
-    );
-}
+    try {
+      const processedLines = await punctuateTextLineByLine(nonEmptyLines);
+      setResult(processedLines);
+      if (isMobile) {
+        setText(processedLines.join("\n")); //changes current text to processed text when screen is mobile sized
+      } 
+    } catch (err) {
+      setError("Failed to process text");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <main className="h-screen overflow-hidden lg:overflow-auto lg:grid-container">
+      <Helmet title="punctuate" />
+      <div className="App h-full">
+        <TextInput
+          onSubmit={handleTextSubmit}
+          text={text}
+          setText={setText}
+          loading={loading}
+        />
+        {isMobile && <ResultOutput result={result} />} 
+        {error && <p className="text-gray-800">{error}</p>}
+      </div>
+      <div className="hidden lg:block">
+        <ResultOutput result={result} />
+      </div>
+    </main>
+  );
+};
 
 export default PunctuateApp;
--- src/useDetectWindowSize.ts
+++ src/useDetectWindowSize.ts
@@ -4,21 +4,21 @@ const useDetectWindowSize = () => {
     const [windowSize, setWindowSize] = useState({
         width: window.innerWidth,
         height: window.innerHeight,
-    });
+    }); // windowSize initialized with the current window size
 
     useEffect(() => {
         const handleWindowResize = () => {
             setWindowSize({
                 width: window.innerWidth,
                 height: window.innerHeight,
-            });
+            }); // function to update the windowSize state with the current window
         };
 
-        window.addEventListener('resize', handleWindowResize);
-        return () => window.removeEventListener('resize', handleWindowResize)
+        window.addEventListener('resize', handleWindowResize); //EventListener calls the handleWindowResize function to update state whenever the window size changes
+        return () => window.removeEventListener('resize', handleWindowResize) // clean up function for when the component unMounts
     }, []);
 
-    return windowSize;
+    return windowSize; 
 }
 
 export default useDetectWindowSize;
